29ca1892158519ccf041c42fbc1f0773384ef3c3
PrabhuUdurg <egorpeshiy@gmail.com>
2023-04-19 11:07:04 +0200

    New commits

--- alien.py
+++ alien.py
@@ -1,14 +1,22 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AliensInvasion:
     def __init__(self):
         # Initialise a game and create game window
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Aliens Absurd")
+
+        self.clock = pygame.time.Clock()  # Frame rate(FPS)
+
+        # Import Settings
+
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
 
     def run_game(self):
 
@@ -19,6 +27,10 @@ class AliensInvasion:
                     sys.exit()
 
             pygame.display.flip()
+            self.clock.tick(60)  # FPS
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
 
 
 if __name__ == '__main__':
