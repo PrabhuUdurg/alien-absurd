4d68915bf6e2173f51388c33d4970eb479a1a7e0
PrabhuUdurg <egorpeshiy@gmail.com>
2023-05-03 19:35:23 +0200

    controller

--- alien.py
+++ alien.py
@@ -1,37 +1,41 @@
 import sys
 import pygame
 from settings import Settings
-
+from ship import Ship
 
 class AliensInvasion:
     def __init__(self):
         # Initialise a game and create game window
         pygame.init()
-        pygame.display.set_caption("Aliens Absurd")
 
         self.clock = pygame.time.Clock()  # Frame rate(FPS)
+
+        self.ship = Ship(self)
 
         # Import Settings
 
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Aliens Absurd")
+
 
     def run_game(self):
 
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            pygame.display.flip()
+
+            
             self.clock.tick(60)  # FPS
-
-            # Redraw the screen during each pass through the loop
+    
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen
             self.screen.fill(self.settings.bg_color)
-
+            self.ship.blitme()
+            pygame.display.flip()
 
 if __name__ == '__main__':
     # Making a game instant and run the game
--- ship.py
+++ ship.py
@@ -8,15 +8,25 @@ class Ship:
         """ Init the ship and adding start position """ 
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        
 
        #Load the ship image and get its rect.
-       self.image = pygame.image.load('ship-al-abs.bmp')
-       self.rect = self.image.get_rect()
+        self.image = pygame.image.load('ship-al-abs.bmp')
+        self.rect = self.image.get_rect()
 
        #Start each new ship at the bottom center of the screen 
-       self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self): 
         """ Draw the ship its current location """ 
         self.screen.blit(self.image, self.rect)
         
+    def _check_events(self):
+        # Respond to keypress and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
