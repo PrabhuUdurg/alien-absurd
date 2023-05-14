63e1e56fdfc59f68c7acc8cc6e8ba7c911b6cdc9
PrabhuUdurg <egorpeshiy@gmail.com>
2023-05-14 22:53:02 +0200

    sunday error

--- alien.py
+++ alien.py
@@ -3,21 +3,18 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AliensInvasion:
     def __init__(self):
         # Initialise a game and create game window
         pygame.init()
-
         self.clock = pygame.time.Clock()  # Frame rate(FPS)
-
-        self.ship = Ship(self)
-
         # Import Settings
-
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Aliens Absurd")
+        self.ship = Ship(self)
 
 
     def run_game(self):
@@ -36,8 +33,19 @@ class AliensInvasion:
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             pygame.display.flip()
+    
+    def _check_events(self):
+        # Respond to keypress and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #Move the ship to the right
+                    self.ship.rect.x += 1
 
 if __name__ == '__main__':
     # Making a game instant and run the game
     ai = AliensInvasion()
     ai.run_game()
+
--- ship.py
+++ ship.py
@@ -21,12 +21,4 @@ class Ship:
         """ Draw the ship its current location """ 
         self.screen.blit(self.image, self.rect)
         
-    def _check_events(self):
-        # Respond to keypress and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT():
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right
-                    self.ship.rect.x += 1
+  
