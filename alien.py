789b09f13849b6dd5aaa3457d995075e0c9a21ff
PrabhuUdurg <egorpeshiy@gmail.com>
2023-05-28 17:55:21 +0200

    Bullets

--- alien.py
+++ alien.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AliensInvasion:
@@ -15,6 +16,7 @@ class AliensInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Aliens Absurd")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     def run_game(self):
@@ -24,6 +26,8 @@ class AliensInvasion:
             
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+            self._update_bullets()
             self._update_screen() 
             self.clock.tick(60)  # Frame rate(FPS)
             
@@ -32,6 +36,8 @@ class AliensInvasion:
     def _update_screen(self):
         # Update images on the screen, and flip to the new screen
             self.screen.fill(self.settings.bg_color)
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
             self.ship.blitme()
             pygame.display.flip()
             
@@ -62,7 +68,25 @@ class AliensInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
-                          
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+            
+    def _fire_bullet(self):
+        # Create a new bullet and add it to the bullets group
+        if len(self.bullets) < self.settings.bullet_allowed:
+                new_bullet = Bullet(self)
+                self.bullets.add(new_bullet)
+                
+    def _update_bullets(self):
+        # Update position of bullets and get rid of old bullets
+        # Update bullet positions
+        self.bullets.update()
+        
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # print(len(self.bullets))
                         
 if __name__ == '__main__':
     # Making a game instant and run the game
